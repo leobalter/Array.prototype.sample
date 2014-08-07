e3c77364f9cba8822482d5ea0e6efeaacfbe6882
ashley williams <heyashleyashley@gmail.com>
2014-08-07 01:31:22 -0400

    update implementation to allow for a single argument
    add conditional to account for relationship between argument and element list
    
    add condition for calling sample on an empty array
    
    incorporate tyler's implementation suggestions and throw errors on bad inputs
    
    fix implemntation to not use coercion

--- reference-implementation/index.js
+++ reference-implementation/index.js
@@ -1,5 +1,22 @@
 "use strict";
 
-Array.prototype.sample = function () {
-  return this[Math.floor(Math.random()*this.length)];
+Array.prototype.sample = function (n) {
+  var randNum, dupe, result;
+  if (arguments.length === 0) {
+    result = this[Math.floor(Math.random()*this.length)];
+  } else if (parseInt(n) !== n) {
+    throw TypeError("argument must be a number");
+  } else if (n <= 0) {
+    throw RangeError("argument must greater than zero");
+  } else {
+    n = n > this.length ? this.length : n;
+    dupe = this.slice();
+    result = [];
+    for(var i=0; i<n; i++) {
+      randNum = Math.floor(Math.random()*dupe.length);
+      result.push(dupe[randNum]);
+      dupe.splice(randNum, 1);
+    }
+  }
+  return result;
 };
